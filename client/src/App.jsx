f46d11f4a9812918bb4972c9a0250d0bb28fd6b8
Arun Bohra <arunbohra12@gmail.com>
2022-10-22 12:34:41 +0530

    🎉 Initialize structure for frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Switch } from 'react-router-dom';
 import Home from './pages/home/home.page';
 
 const App = () => {
   return (
     <div className='App'>
-      <Home />
+      <Switch>
+        <Route path='/' component={Home} />
+        <Route path='/resesarcher-loign' component={Home} />
+        <Route path='/become-a-researcher' component={Home} />
+      </Switch>
     </div>
   );
 };
